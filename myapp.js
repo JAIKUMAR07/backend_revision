0f142b56251747c30f78543c75b9b4d9a7d55291
jaikumar <jaikumardewangan88@gmail.com>
2025-04-02 21:21:37 +0530

    form security ke sath send krna but middle ware bhi aata hai

--- myapp.js
+++ myapp.js
@@ -1,29 +1,26 @@
 const express = require("express");
-
 const app = express();
-
 const morgan = require("morgan");
-
-app.set("view engine", "ejs");
 
 app.use(morgan("dev"));
 
-app.get(
-  "/",
-  (req, res, next) => {
-    const c = 10;
-    const d = 20;
-    console.log(c + d);
-    next();
-  },
-  (req, res) => {
-    res.render("index", { siteName: "My Awesome Site", userName: "John" });
-  }
-);
-1;
+app.set("view engine", "ejs");
 
-app.get("/a", (req, res) => {
-  res.render("index", { siteName: "My Awesome Site", userName: "John" });
+// built in middle ware to decode the data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about page ");
+});
+
+app.post("/get-form-data", (req, res) => {
+  console.log(req.body); // Fixed typo
+  res.send("Data received"); // Fixed spelling
 });
 
 app.listen(3000);
