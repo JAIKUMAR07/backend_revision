5bc846d6e487c8253198494d575c33da30b9218f
jaikumar <jaikumardewangan88@gmail.com>
2025-04-02 21:35:58 +0530

    publcaly files rendor krna without authentication

--- myapp.js
+++ myapp.js
@@ -10,6 +10,8 @@ app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// server ka public file ko allow krwana chalane ke liye
+app.use(express.static("public"));
 app.get("/", (req, res) => {
   res.render("index");
 });
