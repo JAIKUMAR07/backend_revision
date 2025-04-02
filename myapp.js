0df8719c05816419d3642eb30c60461100f682f9
jaikumar <jaikumardewangan88@gmail.com>
2025-04-02 20:46:53 +0530

    middlle ware learning

--- myapp.js
+++ myapp.js
@@ -2,11 +2,24 @@ const express = require("express");
 
 const app = express();
 
+const morgan = require("morgan");
+
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-  res.render("index");
-});
+app.use(morgan("dev"));
+
+app.get(
+  "/",
+  (req, res, next) => {
+    const c = 10;
+    const d = 20;
+    console.log(c + d);
+    next();
+  },
+  (req, res) => {
+    res.render("index", { siteName: "My Awesome Site", userName: "John" });
+  }
+);
 1;
 
 app.get("/a", (req, res) => {
